48b71405eb8d6bf3d793be3dc4b2306782fa4660
Mikas-V <160748424+Mikas-V@users.noreply.github.com>
2025-01-21 23:38:54 +0000

    version: DO NOT PLAY IF YOU HAVE EPILEPSY

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,66 +1,44 @@
-// reminder:
-// reset mouse mode by pressing key with no functionality
-
-
-let value = 0;
-
 function setup() {
-  createCanvas(1000, 700);
-  background(65,65,58);
+  createCanvas(600, 600);
+  background(0);
+  //
+  fill("white");
+  text('Left mouse button to draw; Right mouse button to erase', 50, 50);
   textSize(20);
-  fill(255, 244, 254);
-  text("Abstraction Simulator 101", 30, 30);
-  text("Pressed `e` for an eraser", 30, 50);
-  text("Pressed `s` for saving an image.", 30, 70);
-  text("Pressed `c` for clear", 30, 90);
+  text('WARNING: DO NOT PLAY IF YOU HAVE EPILEPSY', 50, 70);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    noStroke();
-    // Create a p5.Color object.
-    let c = color(255, 128, 128);
-    noStroke();
-
-    // if any key is pressed, turns into eraser
-    if (value == 255) {
-      let c = color(65,65,58);
-      fill(c);
-      ellipse(mouseX, mouseY, 60, 60);
-    }
-    
-    // if you press 's' key and click mouse once, it will save an image (or 10) of your masterpieces
-    else if (value == 250) {
-      save('millionDollarArt.jpg');
-    }
-    
-    // clears the whole canvas
-    else if (value == 200) {
-      setup();
-    }
-    
-    // drawing tool that changes stroke weight based on mouse speed
-    else {
-      c.setAlpha(180);
-      strokeWeight(1);
-      fill(c);
-      distanceMoved = abs(mouseX - pmouseX);
-      ellipse(mouseX, mouseY, distanceMoved);
-      //ellipse(mouseX, mouseY, 35, 60);
-    }
+  if(mouseButton === LEFT){
+    drawMarker();
+  }
+  if(mouseButton === RIGHT){
+  //  let randomColor = random(255);
+ //   background(randomColor);
+    drawEraser();
+  }
+  //ellipse(mouseX, mouseY, 10, 10);
+  //rect(0, 0, 30, 50);
+}
+function drawMarker(){
+  if (mouseIsPressed){
+    //fill(255,200);
+    stroke(random(255),random(255),random(255));
+    line(pmouseX, pmouseY, mouseX, mouseY);
+    //ellipse(mouseX, mouseY, 10, 10);
+    let speed = abs(mouseX - pmouseX);
+    strokeWeight( speed + 2);
   }
 }
-
-
-// detects whether a key is being pressed
-function keyPressed() {
-  if (key === 'e') {
-    value = 255;
-  } else if (key === 's') {
-    value = 250;
-  } else if (key === 'c') {
-    value = 200;
-  } else {
-    value = 0;
-  }
+function drawEraser(){
+  let bgColor = color(random(360), random(100), random(100));
+  background(bgColor);
+  fill("white");
+  text('Left mouse button to draw; Right mouse button to erase', 50, 50);
+  textSize(20);
+  text('WARNING: DO NOT PLAY IF YOU HAVE EPILEPSY', 50, 70);
+  noStroke();
+  ellipse(mouseX, mouseY, 20, 20);
+  //noStroke();
+  fill(bgColor);
 }
