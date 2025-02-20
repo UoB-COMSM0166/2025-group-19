bc0c483854c22a9554804caec53483d0ce52b4de
Mikas Vong <->
2025-02-20 17:08:42 +0000

    updated brick init method

--- docs/controllers/stage/Stage02Controller.js
+++ docs/controllers/stage/Stage02Controller.js
@@ -12,16 +12,18 @@ class Stage02Controller extends StageController {
   }
   initBricks() {
     this.state.bricks = [];
-    for (let i = 0; i < 15; i++) {
-      for (let j = 0; j < 12; j++) {
-        this.state.bricks.push(new Brick(
-          j * 60 + 50,
-          i * 30 + 50,
-          60,
-          30
-        ));
+    console.log("bricks initialized, length: ", this.state.bricks.length); 
+    
+    loadJSON("./models/components/StagePattern/Stage02.json", (data) => {
+      let brickWidth = data.width;
+      let brickHeight = data.height;  
+      
+      console.log("Brick data received:", data, " brickwidth: ", brickWidth, " brickHeight: ", brickHeight); 
+      for (let brickData of data.bricks) {
+        let brick = new Brick(brickData.x, brickData.y, brickWidth, brickHeight, false, brickData.bomb);
+        this.state.bricks.push(brick);
       }
-    }
+    });
   }
 
   goToNextStage() {
