5ae1782d7767b024c93f62797e75a9ef2230bba2
erik1110 <oceanuheart@gmail.com>
2025-02-21 17:30:02 +0000

    feat: add mouse image

--- docs/controllers/stage/Stage01Controller.js
+++ docs/controllers/stage/Stage01Controller.js
@@ -1,24 +1,27 @@
 class Stage01Controller extends StageController {
   constructor(state, view, sidebar, pageController) {
     super(state, view, sidebar, pageController);
-    this.toolDropRate = 0.4; // 40% dropping rate
+    this.toolDropRate = 0.4; // 70% dropping rate
     this.toolProbabilities = {
       ballGrow: 0.5,
       ballShrink: 0.5,
     };
   }
+
+  //Copy this to own Stage0xController
   initBricks() {
     this.state.bricks = [];
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 10; j++) {
-        this.state.bricks.push(new Brick(
-          j * 60 + 50,
-          i * 30 + 50,
-          60,
-          30
-        ));
+    loadJSON("./models/components/StagePattern/Stage01.json", (data) => {
+      let brickWidth = data.width;
+      let brickHeight = data.height;
+      for (let brickData of data.bricks) {
+        let colorValues = data.colour[brickData.colour];
+        let [r, g, b] = colorValues;
+        let brick = new Brick(brickData.x, brickData.y, brickWidth, brickHeight, brickData.bomb, r, g, b);
+        this.state.bricks.push(brick);
+        this.isBricksLoaded = true;
       }
-    }
+    });
   }
 
   goToNextStage() {
