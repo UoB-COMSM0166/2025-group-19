66f0f1e8f7b5155c812a64ccf0b24415a02cb1c1
Mikas Vong <->
2025-02-23 13:17:18 +0000

    Resolved conflict but still need to check

--- docs/controllers/stage/Stage02Controller.js
+++ docs/controllers/stage/Stage02Controller.js
@@ -1,33 +1,6 @@
 class Stage02Controller extends StageController {
   constructor(state, view, sidebar, pageController) {
     super(state, view, sidebar, pageController);
-<<<<<<< HEAD
-    this.toolDropRate = 0.3; // 70% dropping rate
-    this.toolProbabilities = {
-      ballGrow: 0,
-      ballShrink: 0,
-      paddleGrow: 0,
-      paddleMax: 0,
-      paddleShrink: 0,
-      ballSpeedUp: 0,
-      gravityUp: 0.5
-    };
-  }
-  initBricks() {
-    this.state.bricks = [];
-    console.log("bricks initialized, length: ", this.state.bricks.length); 
-    
-    loadJSON("./models/components/StagePattern/Stage02.json", (data) => {
-      let brickWidth = data.width;
-      let brickHeight = data.height;  
-      
-      console.log("Brick data received:", data, " brickwidth: ", brickWidth, " brickHeight: ", brickHeight); 
-      for (let brickData of data.bricks) {
-        let brick = new Brick(brickData.x, brickData.y, brickWidth, brickHeight, false, brickData.bomb);
-        this.state.bricks.push(brick);
-      }
-    });
-=======
     this.toolDropRate = 0.3;
     this.toolProbabilities = {
       ballGrow: 0.1,
@@ -35,12 +8,13 @@ class Stage02Controller extends StageController {
       paddleGrow: 0.1,
       paddleMax: 0.1,
       paddleShrink: 0.1,
+      ballSpeedUp: 0,
+      gravityUp: 0.5
     };
   }
 
   getStageJsonPath() {
     return "./models/components/StagePattern/Stage02.json";
->>>>>>> develop
   }
 
   goToNextStage() {
