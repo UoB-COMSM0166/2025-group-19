876463f63646c7ed25e109a9981f3472a05a7396
erik1110 <oceanuheart@gmail.com>
2025-02-25 23:01:16 +0000

    feat: update rat stage

--- docs/controllers/stage/Stage01Controller.js
+++ docs/controllers/stage/Stage01Controller.js
@@ -3,6 +3,8 @@ class Stage01Controller extends StageController {
     super(state, view, sidebar, pageController);
     this.toolDropRate = 0.5; // 50% dropping rate
     this.toolProbabilities = {
+      ballGrow: 0.3,
+      ballShrink: 0.3,
       infiniteBall: 0.3,
     };
   }
