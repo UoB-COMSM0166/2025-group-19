e068cc701f665ff99fc45dcfd871fc58d771a956
Daisy <Daisy@qq.com>
2025-03-03 11:42:12 +0000

    Add rabbit page pixel

--- docs/controllers/PageController.js
+++ docs/controllers/PageController.js
@@ -22,6 +22,9 @@ class PageController {
         case 'Tiger':
           this.currentPage = new Stage03Controller(state, gameview, sidebar, this);
           break;
+        case 'Rabbit':
+          this.currentPage = new Stage04Controller(state, gameview, sidebar, this);
+          break;
         case 'Dragon':
           this.currentPage = new Stage05Controller(state, gameview, sidebar, this);
           break;
