4758626c01b58049946560290d7697024b59e0a1
Areta <lee.aretafencing@gmail.com>
2025-05-07 15:55:35 +0100

    more adjustments to infinite ball rate

--- docs/models/state/StageState.js
+++ docs/models/state/StageState.js
@@ -43,7 +43,7 @@ class StageState {
       this.toolProbabilities = {
         ballGrow: 0.4,
         ballShrink: 0.7,
-        infiniteBall: 1.1,
+        infiniteBall: 1.2,
         paddleMax: 0.4,
         paddleGrow: 0.6,
         paddleShrink: 0.4,
