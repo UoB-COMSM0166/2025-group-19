705ef6d275924105e8eaa365b5fbd77e6f3abd1b
erik1110 <oceanuheart@gmail.com>
2025-05-11 14:43:56 +0100

    feat: add throttle

--- docs/controllers/SoundController.js
+++ docs/controllers/SoundController.js
@@ -1,15 +1,27 @@
 class SoundController {
-    constructor() {
-      this.isSoundPlaying = false;
+  constructor() {
+    this.isSoundPlaying = false;
+    this.throttledPlaySound = this.throttle(this.playBreakBlockSound, 500);
+  }
+
+  playBreakBlockSound() {
+    if (!this.isSoundPlaying) {
+      this.isSoundPlaying = true;
+      breakBlockSound.play();
+      breakBlockSound.onended(() => {
+        this.isSoundPlaying = false;
+      });
     }
-  
-    playBreakBlockSound() {
-      if (!this.isSoundPlaying) {
-        this.isSoundPlaying = true;
-        breakBlockSound.play();
-        breakBlockSound.onended(() => {
-          this.isSoundPlaying = false;
-        });
+  }
+
+  throttle(func, wait) {
+    let lastTime = 0;
+    return (...args) => {
+      const now = Date.now();
+      if (now - lastTime >= wait) {
+        func(...args);
+        lastTime = now;
       }
-    }
+    };
+  }
 }
